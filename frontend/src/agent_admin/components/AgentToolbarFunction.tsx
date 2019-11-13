a82b3cf2e340abb758cf3b418d9f68be61933e1b
Choe DaeBeom <daebeom.choe@snetsystems.co.kr>
2019-11-13 11:08:10 +0900

    db choe: AgentConfig measure guide display control

--- frontend/src/agent_admin/components/AgentToolbarFunction.tsx
+++ frontend/src/agent_admin/components/AgentToolbarFunction.tsx
@@ -1,68 +1,94 @@
-import React, {PureComponent} from 'react'
-
-import FunctionTooltip from 'src/flux/components/flux_functions_toolbar/FunctionTooltip'
-
-import {ErrorHandling} from 'src/shared/decorators/errors'
+import React, { PureComponent } from "react";
+import FunctionTooltip from "src/flux/components/flux_functions_toolbar/FunctionTooltip";
+import { ErrorHandling } from "src/shared/decorators/errors";
 
 @ErrorHandling
 class AgentToolbarFunction extends PureComponent {
-  private functionRef = React.createRef()
+  private functionRef = React.createRef();
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isActive: false,
-      clickPosition: undefined,
-    }
+      clickPosition: undefined
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
   }
 
   public componentWillReceiveProps(newProps) {
-    if (newProps.refresh) {
-      const boolean = newProps.focusedMeasure === this.functionRef.current
-      this.setState({isActive: boolean})
+    const { refresh } = newProps;
+    const { isActive } = this.state;
+
+    if (refresh === true && isActive === true) {
+      return this.handleOtherClick();
+    }
+
+    if (refresh === false && isActive === true) {
+      return this.handleOtherClick();
+    }
+
+    if (refresh === false && isActive === false) {
+      return this.handleOtherClick();
+    }
+
+    if (refresh) {
+      const boolean = newProps.focusedMeasure === this.functionRef.current;
+      return this.setState({ isActive: boolean });
     }
   }
 
+  componentWillUnmount() {}
+
   render() {
-    const {name, handleFocusedMeasure, focusedMeasure, idx} = this.props
+    const { name, idx } = this.props;
     return (
-      <div className="query-builder--list-item" style={{position: 'relative'}}>
+      <div
+        className="query-builder--list-item"
+        style={{ position: "relative" }}
+      >
         {`${name}`}
         <button
           ref={this.functionRef}
-          className={'btn btn-primary item' + idx}
+          className={"btn btn-primary item" + idx}
           onClick={this.handleFocusing.bind(this)}
         >
           {`help ${idx}`}
         </button>
         {this.tooltip}
       </div>
-    )
+    );
   }
+
+  private handleResize = () => {
+    this.handleOtherClick();
+  };
 
   private handleFocusing = () => {
-    const clickPosition = this.functionRef.current.getBoundingClientRect()
-    this.props.handleFocusedMeasure({clickPosition, refresh: true})
-  }
+    const clickPosition = this.functionRef.current.getBoundingClientRect();
+    this.props.handleFocusedMeasure({ clickPosition, refresh: true });
+  };
 
   private handleOtherClick = () => {
-    this.setState({isActive: false})
-    this.props.handleFocusedMeasure({refresh: false})
-  }
+    this.setState({ isActive: false });
+    this.props.handleFocusedMeasure({ refresh: false });
+  };
 
   private get tooltip(): JSX.Element {
-    const {focusedPosition} = this.props
+    const { focusedPosition } = this.props;
     if (this.state.isActive) {
-      const dummy = {desc: 123, args: 456, example: 678, link: '@link'}
+      const dummy = { desc: 123, args: 456, example: 678, link: "@link" };
       return (
         <FunctionTooltip
           func={dummy}
           onDismiss={this.handleOtherClick}
           tipPosition={focusedPosition}
-          pivot={'left'}
+          pivot={"left"}
         />
-      )
+      );
     }
   }
 }
 
-export default AgentToolbarFunction
+export default AgentToolbarFunction;
--- frontend/src/agent_admin/containers/AgentConfiguration.tsx
+++ frontend/src/agent_admin/containers/AgentConfiguration.tsx
@@ -1,80 +1,79 @@
-import React, {PureComponent} from 'react'
-import _ from 'lodash'
+import React, { PureComponent } from "react";
+import _ from "lodash";
 
 // Components
-import Threesizer from 'src/shared/components/threesizer/Threesizer'
-import AgentTable from 'src/agent_admin/components/AgentTable'
-import FancyScrollbar from 'src/shared/components/FancyScrollbar'
-import FluxEditor from 'src/flux/components/FluxEditor'
-import AgentToolbarFunction from 'src/agent_admin/components/AgentToolbarFunction'
+import Threesizer from "src/shared/components/threesizer/Threesizer";
+import AgentTable from "src/agent_admin/components/AgentTable";
+import FancyScrollbar from "src/shared/components/FancyScrollbar";
+import FluxEditor from "src/flux/components/FluxEditor";
+import AgentToolbarFunction from "src/agent_admin/components/AgentToolbarFunction";
 
-import {ErrorHandling} from 'src/shared/decorators/errors'
+import { ErrorHandling } from "src/shared/decorators/errors";
 
 //const
-import {HANDLE_HORIZONTAL, HANDLE_VERTICAL} from 'src/shared/constants'
+import { HANDLE_HORIZONTAL, HANDLE_VERTICAL } from "src/shared/constants";
 
 interface State {
-  minions: Readonly<[]>
-  proportions: number[]
+  minions: Readonly<[]>;
+  proportions: number[];
 }
 
 @ErrorHandling
 class AgentConfiguration extends PureComponent<State> {
-  private functionRef = React.createRef()
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       measurements: [],
-      minionLog: 'not load log',
+      minionLog: "not load log",
       horizontalProportions: [0.43, 0.57],
       verticalProportions: [0.43, 0.57],
       suggestions: [],
-      draftScriptStatus: {type: 'none', text: ''},
+      draftScriptStatus: { type: "none", text: "" },
       isWizardActive: false,
-      focusedMeasure: '',
+      focusedMeasure: "",
       focusedMeasurePosition: {},
-      refresh: true,
-    }
+      refresh: true
+    };
 
-    this.measurementsTemp = this.measurementsTemp.bind(this)
+    this.measurementsTemp = this.measurementsTemp.bind(this);
   }
 
   public onClickTableRowCall() {
-    return console.log('row Called', this)
+    return console.log("row Called", this);
   }
 
   public onClickActionCall() {
-    return console.log('action Called', this)
+    return console.log("action Called", this);
   }
 
   public onClickSaveCall() {
-    return console.log('Save Called', this)
+    return console.log("Save Called", this);
   }
 
   public onClickTestCall() {
-    return console.log('Test Called', this)
+    return console.log("Test Called", this);
   }
 
   public onClickApplyCall() {
-    return console.log('Apply Called', this)
+    return console.log("Apply Called", this);
   }
 
   public componentDidMount() {
     this.setState({
       measurements: [
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-        'tomcat',
-      ],
-    })
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat",
+        "tomcat"
+      ]
+    });
   }
 
   render() {
@@ -86,35 +85,35 @@ class AgentConfiguration extends PureComponent<State> {
           onResize={this.horizontalHandleResize}
         />
       </div>
-    )
+    );
   }
 
-  private handleFocusedMeasure = ({clickPosition, refresh}) => {
+  private handleFocusedMeasure = ({ clickPosition, refresh }) => {
     if (clickPosition) {
       this.setState({
         focusedMeasure: event.target,
         focusedMeasurePosition: clickPosition,
-        refresh: true,
-      })
+        refresh: true
+      });
     }
 
     if (refresh === false) {
       this.setState({
-        refresh,
-      })
+        refresh
+      });
     }
-  }
+  };
 
   private horizontalHandleResize = (horizontalProportions: number[]) => {
-    this.setState({horizontalProportions})
-  }
+    this.setState({ horizontalProportions });
+  };
 
   private verticalHandleResize = (verticalProportions: number[]) => {
-    this.setState({verticalProportions})
-  }
+    this.setState({ verticalProportions });
+  };
 
   private renderAgentPageTop = () => {
-    const {currentUrl, minions} = this.props
+    const { currentUrl, minions } = this.props;
     return (
       <AgentTable
         currentUrl={currentUrl}
@@ -122,45 +121,45 @@ class AgentConfiguration extends PureComponent<State> {
         onClickTableRow={this.onClickTableRowCall}
         onClickAction={this.onClickActionCall}
       />
-    )
-  }
+    );
+  };
 
   private renderAgentPageBottom = () => {
-    const {minionLog} = this.state
+    const { minionLog } = this.state;
     return (
       <Threesizer
         orientation={HANDLE_VERTICAL}
         divisions={this.verticalDivisions}
         onResize={this.verticalHandleResize}
       />
-    )
-  }
+    );
+  };
 
   private measurementsTemp() {
     const {
       measurements,
       focusedMeasure,
       focusedMeasurePosition,
-      refresh,
-    } = this.state
+      refresh
+    } = this.state;
     return (
       <div className="panel">
         <div className="panel-heading">
           <h2
             className="panel-title"
             style={{
-              width: '100%',
+              width: "100%"
             }}
           >
             measurements
             <div
               style={{
-                color: '#f58220',
-                fontSize: '12px',
-                background: '#232323',
-                padding: '10px',
-                margin: '5px 0px',
-                width: '100%',
+                color: "#f58220",
+                fontSize: "12px",
+                background: "#232323",
+                padding: "10px",
+                margin: "5px 0px",
+                width: "100%"
               }}
             >
               host1-minion1-192.168.0.1
@@ -181,13 +180,13 @@ class AgentConfiguration extends PureComponent<State> {
                     focusedPosition={focusedMeasurePosition}
                     refresh={refresh}
                   />
-                )
+                );
               })}
             </div>
           </FancyScrollbar>
         </div>
       </div>
-    )
+    );
   }
 
   private collectorConfigTemp() {
@@ -199,7 +198,7 @@ class AgentConfiguration extends PureComponent<State> {
             <button
               className="btn btn-inline_block btn-default"
               style={{
-                marginLeft: '5px',
+                marginLeft: "5px"
               }}
               onClick={this.onClickApplyCall}
             >
@@ -211,15 +210,15 @@ class AgentConfiguration extends PureComponent<State> {
         <div className="panel-body">
           <div
             style={{
-              width: '100%',
-              height: '100%',
-              overflow: 'hidden',
-              position: 'relative',
+              width: "100%",
+              height: "100%",
+              overflow: "hidden",
+              position: "relative"
             }}
           >
             <FluxEditor
-              status={{type: 'none', text: ''}}
-              script={'string'}
+              status={{ type: "none", text: "" }}
+              script={"string"}
               visibility={true}
               // suggestions={suggestions}
               // onChangeScript={this.handleChangeDraftScript}
@@ -230,60 +229,60 @@ class AgentConfiguration extends PureComponent<State> {
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   private get horizontalDivisions() {
-    const {horizontalProportions} = this.state
-    const [topSize, bottomSize] = horizontalProportions
+    const { horizontalProportions } = this.state;
+    const [topSize, bottomSize] = horizontalProportions;
 
     return [
       {
-        name: '',
-        handleDisplay: 'none',
+        name: "",
+        handleDisplay: "none",
         headerButtons: [],
         menuOptions: [],
         render: this.renderAgentPageTop,
         headerOrientation: HANDLE_HORIZONTAL,
-        size: topSize,
+        size: topSize
       },
       {
-        name: '',
+        name: "",
         handlePixels: 8,
         headerButtons: [],
         menuOptions: [],
         render: this.renderAgentPageBottom,
         headerOrientation: HANDLE_HORIZONTAL,
-        size: bottomSize,
-      },
-    ]
+        size: bottomSize
+      }
+    ];
   }
 
   private get verticalDivisions() {
-    const {verticalProportions} = this.state
-    const [rightSize, leftSize] = verticalProportions
+    const { verticalProportions } = this.state;
+    const [rightSize, leftSize] = verticalProportions;
 
     return [
       {
-        name: '',
-        handleDisplay: 'none',
+        name: "",
+        handleDisplay: "none",
         headerButtons: [],
         menuOptions: [],
         render: this.measurementsTemp,
         headerOrientation: HANDLE_VERTICAL,
-        size: rightSize,
+        size: rightSize
       },
       {
-        name: '',
+        name: "",
         handlePixels: 8,
         headerButtons: [],
         menuOptions: [],
         render: this.collectorConfigTemp.bind(this),
         headerOrientation: HANDLE_VERTICAL,
-        size: leftSize,
-      },
-    ]
+        size: leftSize
+      }
+    ];
   }
 }
 
-export default AgentConfiguration
+export default AgentConfiguration;
