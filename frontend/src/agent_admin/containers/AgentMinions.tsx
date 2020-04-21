c6c51600f66a4566c18de03e6b1cb09642a8a20d
Jack Kim <taehoon.kim@snetsystems.co.kr>
2020-04-21 09:12:09 +0900

    Jack: refactory notification part

--- frontend/src/agent_admin/containers/AgentMinions.tsx
+++ frontend/src/agent_admin/containers/AgentMinions.tsx
@@ -26,7 +26,7 @@ import {
 
 // Notification
 import {notify as notifyAction} from 'src/shared/actions/notifications'
-import {notifyAgentConnectFailed} from 'src/agent_admin/components/notifications'
+import {notifyAgentConnectFailed} from 'src/shared/copy/notifications'
 
 // Constants
 import {HANDLE_HORIZONTAL} from 'src/shared/constants'
--- frontend/src/shared/copy/notifications.ts
+++ frontend/src/shared/copy/notifications.ts
@@ -889,3 +889,101 @@ export const annotationsError = (message: string): Notification => ({
   ...defaultErrorNotification,
   message,
 })
+
+//  CloudHub AgentPage Sources Notifications
+//  ----------------------------------------------------------------------------
+export const notifyAgentConnectSucceeded = (sourceName: string) => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Agent Connect successfully. ${sourceName}`,
+})
+
+export const notifyAgentConnectFailed = (error: string): Notification => ({
+  ...defaultErrorNotification,
+  message: `Agent Connect Failed, ${error}`,
+})
+
+export const notifyAgentDisconnected = (): Notification => ({
+  ...defaultErrorNotification,
+  message: `Agent Disconnected.`,
+})
+
+export const notifyAgentSucceeded = (sourceName: string) => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector ${sourceName} successfully.`,
+})
+
+export const notifyAgentApplySucceeded = (
+  sourceName: string
+): Notification => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector Configuration ${sourceName} successfully.`,
+})
+
+export const notifyAgentLoadedSucceeded = (
+  sourceName: string
+): Notification => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector Configuration ${sourceName} successfully.`,
+})
+
+export const notifyAgentStopSucceeded = (sourceName: string): Notification => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector Configuration ${sourceName} successfully.`,
+})
+
+export const notifyAgentStartSucceeded = (
+  sourceName: string
+): Notification => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector ${sourceName} successfully.`,
+})
+
+export const notifyAgentAcceptSucceeded = (
+  sourceName: string
+): Notification => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector ${sourceName} successfully.`,
+})
+
+export const notifyAgentRejectSucceeded = (
+  sourceName: string
+): Notification => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector ${sourceName} successfully.`,
+})
+
+export const notifyAgentDeleteSucceeded = (
+  sourceName: string
+): Notification => ({
+  ...defaultSuccessNotification,
+  icon: 'server2',
+  message: `Collector ${sourceName} successfully.`,
+})
+
+export const notifyAgentLoadFailed = (error: Error): Notification => ({
+  ...defaultErrorNotification,
+  message: `Agent Load Failed, ${error}`,
+})
+
+export const notifyAgentAcceptFailed = (error: Error): Notification => ({
+  ...defaultErrorNotification,
+  message: `Agent Accept Failed, ${error}`,
+})
+
+export const notifyAgentRejectFailed = (error: Error): Notification => ({
+  ...defaultErrorNotification,
+  message: `Agent Reject Failed, ${error}`,
+})
+
+export const notifyAgentDeleteFailed = (error: Error): Notification => ({
+  ...defaultErrorNotification,
+  message: `Agent Delete Failed, ${error}`,
+})
