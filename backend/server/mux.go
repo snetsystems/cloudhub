a97d480795dc0c8214ae8023e57e53238714894a
rjm <jaemyeong.roh@snetsystems.co.kr>
2020-07-21 09:20:18 +0900

    jmroh: #226 fix: alert rule message text template parsing (#5228)
    * feat: add go text template parsing endpoint
    * chore(routes): change text validation endpoint name
    * feat: validate kapacitor rule templates
    * test(alerts): remove redundant tests
    * chore(routes): update validate endpoint name
    * test(routes): add validate endpoint to tests
    * test(server): add validation endpoints
    * feat(alerts): change template validation to 204

--- backend/server/mux.go
+++ backend/server/mux.go
@@ -350,6 +350,9 @@ func NewMux(opts MuxOpts, service Service) http.Handler {
 
 	router.GET("/cloudhub/v1/env", EnsureViewer(service.Environment))
 
+	// Validates go templates for the js client
+	router.POST("/cloudhub/v1/validate_text_templates", EnsureViewer(service.ValidateTextTemplate))
+
 	allRoutes := &AllRoutes{
 		Logger:      opts.Logger,
 		StatusFeed:  opts.StatusFeedURL,
