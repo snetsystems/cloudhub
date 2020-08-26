d23757c3f13509bdc356fa543fd22c29a5c78635
Choe DaeBeom <daebeom.choe@snetsystems.co.kr>
2020-08-26 17:02:04 +0900

    dbchoe: charts layout rendererr JSX modified

--- frontend/src/hosts/components/ChartsLayoutRenderer.tsx
+++ frontend/src/hosts/components/ChartsLayoutRenderer.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
-import classnames from 'classnames'
 
+// Components
+import {
+  CellName,
+  HeadingBar,
+  PanelHeader,
+  Panel,
+  PanelBody,
+} from 'src/addon/128t/reusable/layout'
 import FancyScrollbar from 'src/shared/components/FancyScrollbar'
 import LayoutVMRenderer from 'src/shared/components/LayoutVMRenderer'
+
+// Types
+import {TimeRange, Cell, Template, Source} from 'src/types'
+import {vmParam} from 'src/hosts/containers/VMHostsPage'
+
+interface ChartsProps {
+  source: Source
+  layoutCells: Cell[]
+  tempVars: Template[]
+  timeRange: TimeRange
+  manualRefresh: number
+  hostID: string
+  isVMware: boolean
+  vmParam: vmParam
+  vmParentChartField: string
+  vmParentName: string
+}
+
+interface DefaultProps {
+  isEditable: boolean
+  cellTextColor: string
+  cellBackgroundColor: string
+}
+
+type Props = DefaultProps & ChartsProps
 
 const ChartsLayoutRenderer = ({
   source,
@@ -15,33 +47,48 @@ const ChartsLayoutRenderer = ({
   vmParam,
   vmParentChartField,
   vmParentName,
-}) => {
+  isEditable,
+  cellTextColor,
+  cellBackgroundColor,
+}: Props) => {
   return (
-    <FancyScrollbar
-      className={classnames({
-        'page-contents': true,
-        // 'presentation-mode': inPresentationMode,
-      })}
-    >
-      <div className="container-fluid full-width dashboard">
-        <LayoutVMRenderer
-          source={source}
-          sources={[source]}
-          isStatusPage={false}
-          isStaticPage={true}
-          isEditable={false}
-          cells={layoutCells}
-          templates={tempVars}
-          timeRange={timeRange}
-          manualRefresh={manualRefresh}
-          host={hostID}
-          isVMware={isVMware}
-          vmParam={vmParam}
-          vmParentChartField={vmParentChartField}
-          vmParentName={vmParentName}
-        />
-      </div>
-    </FancyScrollbar>
+    <div className={`charts-body`}>
+      <Panel>
+        <PanelHeader isEditable={isEditable}>
+          <CellName
+            cellTextColor={cellTextColor}
+            cellBackgroundColor={cellBackgroundColor}
+            value={[]}
+            name={`Charts`}
+            sizeVisible={false}
+          />
+          <HeadingBar
+            isEditable={isEditable}
+            cellBackgroundColor={cellBackgroundColor}
+          />
+        </PanelHeader>
+        <PanelBody>
+          <FancyScrollbar>
+            <LayoutVMRenderer
+              source={source}
+              sources={[source]}
+              isStatusPage={false}
+              isStaticPage={true}
+              isEditable={false}
+              cells={layoutCells}
+              templates={tempVars}
+              timeRange={timeRange}
+              manualRefresh={manualRefresh}
+              host={hostID}
+              isVMware={isVMware}
+              vmParam={vmParam}
+              vmParentChartField={vmParentChartField}
+              vmParentName={vmParentName}
+            />
+          </FancyScrollbar>
+        </PanelBody>
+      </Panel>
+    </div>
   )
 }
 
