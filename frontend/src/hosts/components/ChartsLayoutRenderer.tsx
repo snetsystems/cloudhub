dffa760c53e71d91f65c93679be2f59daa68a6b7
Choe DaeBeom <daebeom.choe@snetsystems.co.kr>
2020-08-28 14:14:43 +0900

    dbchoe: Added resize observer in ChartsLayoutRenderer

--- frontend/src/hosts/components/ChartsLayoutRenderer.tsx
+++ frontend/src/hosts/components/ChartsLayoutRenderer.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import ReactObserver from 'react-resize-observer'
+import _ from 'lodash'
 
 // Components
 import {
@@ -10,6 +12,9 @@ import {
 } from 'src/addon/128t/reusable/layout'
 import FancyScrollbar from 'src/shared/components/FancyScrollbar'
 import LayoutVMRenderer from 'src/shared/components/LayoutVMRenderer'
+
+// Util
+import {WindowResizeEventTrigger} from 'src/shared/utils/trigger'
 
 // Types
 import {TimeRange, Cell, Template, Source} from 'src/types'
@@ -51,6 +56,14 @@ const ChartsLayoutRenderer = ({
   cellTextColor,
   cellBackgroundColor,
 }: Props) => {
+  const debouncedFit = _.debounce(() => {
+    WindowResizeEventTrigger()
+  }, 250)
+
+  const handleOnResize = (): void => {
+    debouncedFit()
+  }
+
   return (
     <div className={`charts-body`}>
       <Panel>
@@ -68,24 +81,27 @@ const ChartsLayoutRenderer = ({
           />
         </PanelHeader>
         <PanelBody>
-          <FancyScrollbar>
-            <LayoutVMRenderer
-              source={source}
-              sources={[source]}
-              isStatusPage={false}
-              isStaticPage={true}
-              isEditable={false}
-              cells={layoutCells}
-              templates={tempVars}
-              timeRange={timeRange}
-              manualRefresh={manualRefresh}
-              host={hostID}
-              isVMware={isVMware}
-              vmParam={vmParam}
-              vmParentChartField={vmParentChartField}
-              vmParentName={vmParentName}
-            />
-          </FancyScrollbar>
+          <div style={{position: 'relative', width: '100%', height: '100%'}}>
+            <ReactObserver onResize={handleOnResize} />
+            <FancyScrollbar>
+              <LayoutVMRenderer
+                source={source}
+                sources={[source]}
+                isStatusPage={false}
+                isStaticPage={true}
+                isEditable={false}
+                cells={layoutCells}
+                templates={tempVars}
+                timeRange={timeRange}
+                manualRefresh={manualRefresh}
+                host={hostID}
+                isVMware={isVMware}
+                vmParam={vmParam}
+                vmParentChartField={vmParentChartField}
+                vmParentName={vmParentName}
+              />
+            </FancyScrollbar>
+          </div>
         </PanelBody>
       </Panel>
     </div>
