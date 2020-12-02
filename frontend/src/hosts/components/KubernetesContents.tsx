0cdc25b9954659452731d431d6884cafc3e3ba62
Choe DaeBeom <daebeom.choe@snetsystems.co.kr>
2020-12-02 09:30:02 +0900

    dbchoe: component separation

--- frontend/src/hosts/components/KubernetesContents.tsx
+++ frontend/src/hosts/components/KubernetesContents.tsx
@@ -1,7 +1,6 @@
 // Library
-import React, {PureComponent, createRef} from 'react'
+import React, {PureComponent} from 'react'
 import _ from 'lodash'
-import * as d3 from 'd3'
 
 // Component
 import Threesizer from 'src/shared/components/threesizer/Threesizer'
@@ -13,12 +12,12 @@ import {KubernetesItem} from 'src/hosts/containers/KubernetesPage'
 import KubernetesBasicsTable from 'src/hosts/components/KubernetesBasicsTable'
 import KubernetesRawData from 'src/hosts/components/KubernetesRawData'
 import KubernetesTooltip from 'src/hosts/components/KubernetesTooltip'
+import KubernetesHexagon from 'src/hosts/components/KubernetesHexagon'
 
 // Constants
 import {HANDLE_VERTICAL} from 'src/shared/constants'
 import {KUBERNETES_BASICS_TABLE_SIZE} from 'src/hosts/constants/tableSizing'
-import chroma from 'chroma-js'
-
+import {kubernetesStatusColor} from 'src/hosts/constants/color'
 interface Props {
   handleOnSetActiveEditorTab: (tab: string) => void
   handleOnClickPodName: () => void
@@ -30,7 +29,7 @@ interface Props {
   activeTab: string
   script: string
   height: number
-  focuseNode: {}
+  focuseNode: string
   isToolipActive: boolean
   toolipPosition: {top: number; right: number; left: number}
   tooltipNode: {name: string; cpu: number; memory: number}
@@ -41,286 +40,9 @@ interface Props {
 interface State {}
 
 class KubernetesContents extends PureComponent<Props, State> {
-  private myRef = createRef<HTMLDivElement>()
-  private containerStyles = {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#292933',
-  }
-
-  private kubernetesStatusColor = chroma
-    .scale(['#30e7f1', '#00cc2c', '#ff9e00', '#ff0000'])
-    .mode('lrgb')
-
-  private clusterTypeColorset = {
-    ClusterRoles: '#0033cc',
-    CR: '#0033cc',
-    ClusterRoleBindings: '#0033cc',
-    CRB: '#0033cc',
-    Namespace: '#7f7f7f',
-    Service: '#0033cc',
-    SVC: '#0033cc',
-    Secrets: '#ffd966',
-    SR: '#ffd966',
-    ServiceAccounts: '#0033cc',
-    SA: '#0033cc',
-    ReplicaSet: '#e2f0d9',
-    RS: '#e2f0d9',
-    Deployment: '#ffd966',
-    DP: '#ffd966',
-    Node: '#2e75b6',
-    Pod: '#2e75b6',
-    Job: '#fed2d2',
-    CronJob: '#fed2d2',
-    CJ: '#0033cc',
-    Ingress: '#0033cc',
-    IGS: '#0033cc',
-    ReplicationController: '#9dc3e6',
-    RC: '#9dc3e6',
-    Configmaps: '#0033cc',
-    CM: '#0033cc',
-    Roles: '#a27bb3',
-    RL: '#a27bb3',
-    RoleBindings: '#a27bb3',
-    RB: '#a27bb3',
-    DaemonSet: '#74b8ba',
-    DS: '#74b8ba',
-  }
-
   constructor(props: Props) {
     super(props)
-    this.state = {
-      toolipPosition: undefined,
-    }
-  }
-
-  public componentDidUpdate(prevProps: Props) {
-    if (
-      this.props.kubernetesItem &&
-      prevProps.kubernetesItem !== this.props.kubernetesItem
-    ) {
-      this.drawChart()
-    }
-  }
-
-  public drawChart = () => {
-    const _this = this
-    const dimensions = _this.myRef.current.getBoundingClientRect()
-
-    const data = d3
-      .pack()
-      .size([dimensions.width, dimensions.height])
-      .padding(30)(
-      d3
-        .hierarchy(this.props.kubernetesItem)
-        .sum(d => d.value)
-        .sort((a, b) => b.value - a.value)
-    )
-    const SQRT3 = Math.sqrt(3)
-    const hexagonPoly = [
-      [0, -1],
-      [SQRT3 / 2, 0.5],
-      [0, 1],
-      [-SQRT3 / 2, 0.5],
-      [-SQRT3 / 2, -0.5],
-      [0, -1],
-      [SQRT3 / 2, -0.5],
-    ]
-    const generateHexagon = hexRadius => {
-      const hexagonPath =
-        'm' +
-        hexagonPoly
-          .map(function(p) {
-            return [p[0] * hexRadius, p[1] * hexRadius].join(',')
-          })
-          .join('l') +
-        'z'
-      return hexagonPath
-    }
-
-    const circle = d3
-      .arc()
-      .innerRadius(0)
-      .outerRadius(d => d)
-      .startAngle(-Math.PI)
-      .endAngle(Math.PI)
-
-    const svg = d3
-      .create('svg')
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .style('font', '10px sans-serif')
-      .style('overflow', 'visible')
-      .attr('text-anchor', 'middle')
-
-    const node = svg
-      .append('g')
-      .attr('pointer-events', 'all')
-      .selectAll('g')
-      .data(data.descendants().slice(1))
-      .join('g')
-      .attr('transform', d => `translate(${d.x},${d.y})`)
-
-    node
-      .append('path')
-      .attr('id', d => d.data.name)
-      .attr('d', d => circle(d.r + 4))
-      .attr('display', 'none')
-
-    node
-      .filter(d => d.height !== 0)
-      .append('circle')
-      .attr('class', 'nodeWrapper')
-      .attr('r', d => d.r)
-      .attr('fill', d => _this.clusterTypeColorset[d.data.type])
-      .attr('data-type', d => d.data.type)
-      .attr('pointer-events', d => (d.children ? 'all' : 'none'))
-      .on('click', function() {
-        d3.select(this).attr(
-          'stroke',
-          chroma(
-            _this.clusterTypeColorset[d3.select(this).attr('data-type')]
-          ).darken(3)
-        )
-        d3.select(this).attr('stroke-width', '3px')
-        d3.select(this).attr(
-          'fill',
-          chroma(
-            _this.clusterTypeColorset[d3.select(this).attr('data-type')]
-          ).darken()
-        )
-      })
-      .on('mouseout', function() {
-        d3.select(this).attr('stroke', null)
-        d3.select(this).attr(
-          'fill',
-          _this.clusterTypeColorset[d3.select(this).attr('data-type')]
-        )
-      })
-    node
-      .filter(d => d.height === 0)
-      .on('mouseleave', function() {
-        const path = this.children[1]
-        const target = d3.select(path)
-        const cpu = target.attr('data-cpu')
-        const memory = target.attr('data-memory')
-        const pick = cpu > memory ? cpu : memory
-
-        target.attr('fill', _this.kubernetesStatusColor(pick / 100))
-      })
-      .append('path')
-      .attr('class', 'hexagon')
-      .attr('d', d => generateHexagon(d.r + 5))
-      .attr('stroke', 'black')
-      .attr('fill', d =>
-        d.children
-          ? 'none'
-          : (() => {
-              const cpu = d.data.cpu
-              const memory = d.data.memory
-              const pick = cpu > memory ? cpu : memory
-
-              return _this.kubernetesStatusColor(pick / 100)
-            })()
-      )
-      .attr('data-cpu', d => d.data.cpu)
-      .attr('data-memory', d => d.data.memory)
-      .attr('data-name', d => d.data.name)
-      .attr('id', d => 'Node' + d.data.name)
-      .attr('data-relation', d => d.data?.relationNodes)
-      .on('mouseover', function() {
-        _this.props.handleOpenTooltip(this)
-
-        d3.selectAll('.relation-focuse:not(.pined)').classed(
-          'relation-focuse',
-          false
-        )
-
-        d3.select(this).classed('relation-focuse', true)
-
-        let relation = d3.select(this).attr('data-relation')
-        if (relation) {
-          _.map(relation.split(','), node => {
-            d3.select(`#Node${node}`).classed('relation-focuse', true)
-          })
-        }
-      })
-      .on('mouseleave', function() {
-        _this.props.handleCloseTooltip()
-        const target = d3.select(this)
-
-        const isPined = target.attr('class').match(/pined/g)
-
-        if (isPined) {
-          return
-        }
-        target.classed('relation-focuse', false)
-
-        const relation = target.attr('data-relation')
-        if (relation) {
-          _.forEach(relation.split(','), node => {
-            d3.select(`#Node${node}`).classed('relation-focuse', false)
-            d3.select(`#Node${node}`).classed('pined', false)
-          })
-        }
-      })
-      .on('click', function() {
-        _this.props.handleOnClickVisualizePod(d3.select(this).attr('data-name'))
-      })
-      .on('dblclick', function() {
-        _this.tooglePinedTooltip(this)
-      })
-
-    node
-      .filter(d => d.height !== 0)
-      .append('text')
-      .attr('fill', d => (d.height == 2 ? 'white' : 'black'))
-      .append('textPath')
-      .attr('xlink:href', d => '#' + d.data.name)
-      .attr('startOffset', '50%')
-      .attr('font-size', d => (d.height == 2 ? '15px' : '12px'))
-      .attr('data-name', d => d.data.name)
-      .text(d => d.data.name)
-
-    const autoBox = () => {
-      this.myRef.current.appendChild(svg.node())
-      const {x, y, width, height} = svg.node().getBBox()
-      this.myRef.current.removeChild(svg.node())
-      return [x, y, width, height]
-    }
-
-    return this.myRef.current.append(svg.attr('viewBox', `${autoBox()}`).node())
-  }
-
-  private tooglePinedTooltip = target => {
-    const pinTarget = d3.select(target)
-    const beforePinTarget = d3.select('.pined')
-    const isPined = !pinTarget.attr('class').match(/pined/g)
-    const isBeforePined = !beforePinTarget.empty()
-
-    if (isBeforePined) {
-      beforePinTarget.classed('pined', false)
-      beforePinTarget.classed('relation-focuse', false)
-
-      const beforeRelation = beforePinTarget.attr('data-relation')
-      if (beforeRelation) {
-        _.forEach(beforeRelation.split(','), node => {
-          d3.select(`#Node${node}`).classed('relation-focuse', false)
-          d3.select(`#Node${node}`).classed('pined', false)
-        })
-      }
-    }
-
-    pinTarget.classed('pined', isPined)
-    pinTarget.classed('relation-focuse', isPined)
-
-    const relation = pinTarget.attr('data-relation')
-    if (relation) {
-      _.forEach(relation.split(','), node => {
-        d3.select(`#Node${node}`).classed('relation-focuse', isPined)
-        d3.select(`#Node${node}`).classed('pined', isPined)
-      })
-    }
+    this.state = {}
   }
 
   public render() {
@@ -362,7 +84,17 @@ class KubernetesContents extends PureComponent<Props, State> {
     return (
       <FancyScrollbar>
         <div style={{width: '100%', height: '100%'}}>
-          <div style={this.containerStyles} ref={this.myRef}></div>
+          <KubernetesHexagon
+            data={this.props.kubernetesItem}
+            focuseNode={this.props.focuseNode}
+            handleOnSetActiveEditorTab={this.props.handleOnSetActiveEditorTab}
+            handleOnClickPodName={this.props.handleOnClickPodName}
+            handleOnClickVisualizePod={this.props.handleOnClickVisualizePod}
+            handleResize={this.props.handleResize}
+            handleOpenTooltip={this.props.handleOpenTooltip}
+            handleCloseTooltip={this.props.handleCloseTooltip}
+          />
+
           {this.tooltip}
         </div>
         <div> Charts Layout </div>
@@ -372,11 +104,12 @@ class KubernetesContents extends PureComponent<Props, State> {
 
   private get tooltip() {
     if (this.props.isToolipActive) {
+      console.log(this.props.tooltipNode)
       return (
         <KubernetesTooltip
           tipPosition={this.props.toolipPosition}
           tooltipNode={this.props.tooltipNode}
-          statusColor={this.kubernetesStatusColor}
+          statusColor={kubernetesStatusColor}
         />
       )
     }
---
+++ frontend/src/hosts/components/KubernetesHexagon.tsx
@@ -0,0 +1,223 @@
+// Libraries
+import React, {PureComponent, createRef} from 'react'
+import * as d3 from 'd3'
+
+// Components
+import PageSpinner from 'src/shared/components/PageSpinner'
+
+// Constants
+import {
+  kubernetesStatusColor,
+  clusterTypeColorset,
+} from 'src/hosts/constants/color'
+
+interface Props {
+  handleOnSetActiveEditorTab: (tab: string) => void
+  handleOnClickPodName: () => void
+  handleOnClickVisualizePod: (target: string) => void
+  handleResize: (proportions: number[]) => void
+  handleOpenTooltip: (target: any) => void
+  handleCloseTooltip: () => void
+  data: any
+  focuseNode: string
+}
+
+interface State {}
+
+class KubernetesHexagon extends PureComponent<Props, State> {
+  private containerStyles = {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#292933',
+  }
+
+  private ref = createRef<HTMLDivElement>()
+
+  constructor(props: Props) {
+    super(props)
+  }
+
+  public componentDidUpdate() {
+    const removeGroup = d3.select('svg.kubernetes-svg').data(this.props.data)
+    removeGroup.exit().remove()
+
+    this.drawChart()
+  }
+
+  public render() {
+    return this.props.data ? (
+      <div ref={this.ref} style={this.containerStyles} />
+    ) : (
+      <PageSpinner />
+    )
+  }
+
+  private drawChart = () => {
+    const _this = this
+    const {onMouseClick, onMouseDBClick, onMouseOver, onMouseLeave} = _this
+    const {width, height} = _this.ref.current.getBoundingClientRect()
+
+    const data = d3
+      .pack()
+      .size([width, height])
+      .padding(30)(
+      d3
+        .hierarchy(this.props.data)
+        .sum(d => d.value)
+        .sort((a, b) => b.value - a.value)
+    )
+    const SQRT3 = Math.sqrt(3)
+    const hexagonPoly = [
+      [0, -1],
+      [SQRT3 / 2, 0.5],
+      [0, 1],
+      [-SQRT3 / 2, 0.5],
+      [-SQRT3 / 2, -0.5],
+      [0, -1],
+      [SQRT3 / 2, -0.5],
+    ]
+    const generateHexagon = hexRadius => {
+      const hexagonPath =
+        'm' +
+        hexagonPoly
+          .map(function(p) {
+            return [p[0] * hexRadius, p[1] * hexRadius].join(',')
+          })
+          .join('l') +
+        'z'
+      return hexagonPath
+    }
+
+    const circle = d3
+      .arc()
+      .innerRadius(0)
+      .outerRadius(d => d)
+      .startAngle(-Math.PI)
+      .endAngle(Math.PI)
+
+    const svg = d3
+      .create('svg')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .style('font', '10px sans-serif')
+      .style('overflow', 'visible')
+      .attr('text-anchor', 'middle')
+      .classed('kubernetes-svg', true)
+
+    const node = svg
+      .append('g')
+      .attr('pointer-events', 'all')
+      .classed('top-group', true)
+      .selectAll('g')
+      .data(data.descendants().slice(1))
+      .join('g')
+      .classed('group', true)
+      .attr('transform', d => `translate(${d.x},${d.y})`)
+
+    node
+      .append('path')
+      .attr('id', d => d.data.name)
+      .attr('d', d => circle(d.r + 4))
+      .attr('display', 'none')
+
+    node
+      .filter(d => d.height !== 0)
+      .append('circle')
+      .attr('class', 'nodeWrapper')
+      .attr('r', d => d.r)
+      .attr('fill', d => clusterTypeColorset[d.data.type])
+      .attr('data-type', d => d.data.type)
+      .attr('pointer-events', d => (d.children ? 'all' : 'none'))
+      .on('mouseover', function() {
+        onMouseOver(this)
+      })
+      .on('mouseleave', function() {
+        onMouseLeave(this)
+      })
+
+    node
+      .filter(d => d.height === 0)
+      .on('mouseleave', function() {
+        const path = this.children[1]
+        const target = d3.select(path)
+        const cpu = target.attr('data-cpu')
+        const memory = target.attr('data-memory')
+        const pick = cpu > memory ? cpu : memory
+
+        target.attr('fill', kubernetesStatusColor(pick / 100))
+      })
+      .append('path')
+      .attr('class', 'hexagon')
+      .attr('d', d => generateHexagon(d.r + 5))
+      .attr('stroke', 'black')
+      .attr('fill', d =>
+        d.children
+          ? 'none'
+          : (() => {
+              const cpu = d.data.cpu
+              const memory = d.data.memory
+              const pick = cpu > memory ? cpu : memory
+
+              return kubernetesStatusColor(pick / 100)
+            })()
+      )
+      .attr('data-cpu', d => d.data.cpu)
+      .attr('data-memory', d => d.data.memory)
+      .attr('data-name', d => d.data.name)
+      .attr('id', d => 'Node' + d.data.name)
+      .on('mouseover', function() {
+        onMouseOver(this)
+      })
+      .on('mouseleave', function() {
+        onMouseLeave(this)
+      })
+      .on('click', function() {
+        onMouseClick(this)
+      })
+      .on('dblclick', function() {
+        onMouseDBClick(this)
+      })
+
+    node
+      .filter(d => d.height !== 0)
+      .append('text')
+      .attr('fill', d => (d.height == 2 ? 'white' : 'black'))
+      .append('textPath')
+      .attr('xlink:href', d => '#' + d.data.name)
+      .attr('startOffset', '50%')
+      .attr('font-size', d => (d.height == 2 ? '15px' : '12px'))
+      .attr('data-name', d => d.data.name)
+      .text(d => d.data.name)
+
+    const autoBox = () => {
+      this.ref.current.appendChild(svg.node())
+      const {x, y, width, height} = svg.node().getBBox()
+      this.ref.current.removeChild(svg.node())
+      return [x, y, width, height]
+    }
+
+    this.ref.current.append(svg.attr('viewBox', `${autoBox()}`).node())
+  }
+
+  private onMouseClick = (target: SVGSVGElement) => {
+    // focuseNode 설정
+    this.props.handleOnClickVisualizePod(d3.select(target).attr('data-name'))
+  }
+
+  private onMouseDBClick = (target: SVGSVGElement) => {
+    // 고정
+    console.log('dbclick')
+  }
+
+  private onMouseOver = (target: SVGSVGElement) => {
+    // 툴팁 열기
+    this.props.handleOpenTooltip(target)
+  }
+
+  private onMouseLeave = (target: SVGSVGElement) => {
+    // 툴팁 닫기
+    this.props.handleCloseTooltip()
+  }
+}
+
+export default KubernetesHexagon
