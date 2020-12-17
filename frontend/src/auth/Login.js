e33b4a13e6c32a8ba4eafb01c7051ec52996d941
Choidaeboem <chleoqja0125@nate.com>
2020-12-17 18:47:42 +0900

    dbchoe: added login & sign up UI

--- frontend/src/auth/Login.js
+++ frontend/src/auth/Login.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 
-import Notifications from 'shared/components/Notifications'
-
+import {Radio, ButtonShape} from 'src/reusable_ui'
+import Notifications from 'src/shared/components/Notifications'
 import PageSpinner from 'src/shared/components/PageSpinner'
-import SplashPage from 'shared/components/SplashPage'
+import SplashPage from 'src/shared/components/SplashPage'
 
 const VERSION = process.env.npm_package_version
 
@@ -13,6 +13,42 @@ const Login = ({authData: {auth}}) => {
     return <PageSpinner />
   }
 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [activeEditorTab, setActiveEditorTab] = useState('Login')
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value)
+  }
+
+  const onChangePassword = e => {
+    setPassword(e.target.value)
+  }
+
+  const onChangePasswordConfirm = e => {
+    setPasswordConfirm(e.target.value)
+  }
+
+  const onClickLogin = () => {
+    console.log('try log in')
+  }
+
+  const onClickSignUp = () => {
+    console.log('try sign up')
+  }
+
+  const onSetActiveEditorTab = tab => {
+    setActiveEditorTab(tab)
+  }
+
+  useEffect(() => {
+    setEmail('')
+    setPassword('')
+    setPasswordConfirm('')
+  }, [activeEditorTab])
+
+  const isSign = activeEditorTab === 'SignUp'
   return (
     <div>
       <Notifications />
@@ -26,13 +62,90 @@ const Login = ({authData: {auth}}) => {
         <p>
           <b>{VERSION}</b> / Real-Time Applications Monitoring
         </p>
-        {auth.links &&
-          auth.links.map(({name, login, label}) => (
-            <a key={name} className="btn btn-primary" href={login}>
-              <span className={`icon ${name}`} />
-              Log in with {label}
-            </a>
-          ))}
+        <div className={'auth-area'}>
+          <Radio shape={ButtonShape.StretchToFit} customClass={'auth-radio'}>
+            <Radio.Button
+              id="auth-log-in"
+              titleText="Login"
+              value="Login"
+              active={activeEditorTab === 'Login'}
+              onClick={onSetActiveEditorTab}
+            >
+              Login
+            </Radio.Button>
+            <Radio.Button
+              id="auth-sign-up"
+              titleText="SignUp"
+              value="SignUp"
+              active={activeEditorTab === 'SignUp'}
+              onClick={onSetActiveEditorTab}
+            >
+              Sign up
+            </Radio.Button>
+          </Radio>
+
+          <div className="form-group auth-login">
+            <input
+              className="form-control"
+              id="user-email"
+              type="text"
+              placeholder={'yours@example.com'}
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </div>
+          <div className="form-group auth-login">
+            <input
+              className="form-control"
+              id="user-password"
+              type="password"
+              placeholder={'password'}
+              value={password}
+              onChange={onChangePassword}
+            />
+          </div>
+          {isSign ? (
+            <div className="form-group auth-login">
+              <input
+                className="form-control"
+                id="user-password-confirm"
+                type="password"
+                placeholder={'password confirm'}
+                value={passwordConfirm}
+                onChange={onChangePasswordConfirm}
+              />
+            </div>
+          ) : (
+            <span>Forgot your password?</span>
+          )}
+          <div className={'auth-button-bar'}>
+            {isSign ? (
+              <button
+                className="btn btn-primary btn-sm col-md-12"
+                onClick={onClickSignUp}
+              >
+                Sign Up
+              </button>
+            ) : (
+              <button
+                className="btn btn-primary btn-sm col-md-12"
+                onClick={onClickLogin}
+              >
+                Login
+              </button>
+            )}
+          </div>
+          <div className="hr-label">
+            <span className="hr-label__text">OR</span>
+          </div>
+          {auth.links &&
+            auth.links.map(({name, login, label}) => (
+              <a key={name} className="btn btn-primary auth-login" href={login}>
+                <span className={`icon ${name}`} />
+                Log in with {label}
+              </a>
+            ))}
+        </div>
       </SplashPage>
     </div>
   )
