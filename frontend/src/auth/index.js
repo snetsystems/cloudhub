d7eee699cba552b6c6e0b72467434039b078bb8f
Choidaeboem <chleoqja0125@nate.com>
2020-12-23 10:31:51 +0900

    dbchoe: add component in auth index

--- frontend/src/auth/index.js
+++ frontend/src/auth/index.js
@@ -1,6 +1,15 @@
 import Login from './Login'
+import PasswordReset from './PasswordReset'
+import PasswordChange from './PasswordChange'
 import Purgatory from './Purgatory'
 
 import {UserIsAuthenticated, UserIsNotAuthenticated} from './Authenticated'
 
-export {Login, Purgatory, UserIsAuthenticated, UserIsNotAuthenticated}
+export {
+  Login,
+  PasswordReset,
+  PasswordChange,
+  Purgatory,
+  UserIsAuthenticated,
+  UserIsNotAuthenticated,
+}
