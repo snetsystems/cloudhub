f670e9ad6e5fe2056a12594cc6ffc8e6b81de9a6
Choe DaeBeom <daebeom.choe@snetsystems.co.kr>
2021-01-20 15:16:42 +0900

    dbchoe: splash option add

--- frontend/src/shared/components/SplashPage.tsx
+++ frontend/src/shared/components/SplashPage.tsx
@@ -5,28 +5,40 @@ import {InjectedRouter} from 'react-router'
 interface Props {
   router: InjectedRouter
   children: ReactElement<any>
+  isShowCopy: boolean
+  isShowLogo: boolean
 }
 
-const SplashPage: SFC<Props> = ({router = null, children}) => {
+const SplashPage: SFC<Props> = ({
+  router = null,
+  isShowLogo = true,
+  isShowCopy = true,
+  children,
+}) => {
   return (
     <div className="auth-page">
       <div className="auth-box">
-        <div
-          className={classnames('auth-logo', {
-            'go-home': router,
-          })}
-          onClick={() => {
-            router && router.push('/')
-          }}
-        />
+        {isShowLogo && (
+          <div
+            className={classnames('auth-logo', {
+              'go-home': router,
+            })}
+            onClick={() => {
+              router && router.push('/')
+            }}
+          />
+        )}
 
         {children}
       </div>
-      <p className="auth-credits">
-        Copyright by &copy;Snetsystems Co., Ltd. (Derived by{' '}
-        <span className="icon cubo-uniform" />
-        Chronograf)
-      </p>
+      {isShowCopy && (
+        <p className="auth-credits">
+          Copyright by &copy;Snetsystems Co., Ltd. (Derived by{' '}
+          <span className="icon cubo-uniform" />
+          Chronograf)
+        </p>
+      )}
+
       <div className="auth-image" />
     </div>
   )
