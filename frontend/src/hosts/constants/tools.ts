2764e913116152f9402d204a1d3f2e2d1d192d98
Choe DaeBeom <daebeom.choe@snetsystems.co.kr>
2021-02-26 15:42:06 +0900

    dbchoe: add tool obejct

---
+++ frontend/src/hosts/constants/tools.ts
@@ -0,0 +1,100 @@
+export const toolbarCommandObject = [
+  {
+    actionName: 'groupOrUngroup',
+    label: '(Un)group',
+    imgSrc: 'images/group.png',
+  },
+  {
+    actionName: 'delete',
+    label: 'Delete',
+    imgSrc: 'images/delete2.png',
+  },
+  {
+    actionName: 'cut',
+    label: 'Cut',
+    imgSrc: 'images/cut.png',
+  },
+  {
+    actionName: 'copy',
+    label: 'Copy',
+    imgSrc: 'images/copy.png',
+  },
+  {
+    actionName: 'paste',
+    label: 'Paste',
+    imgSrc: 'images/paste.png',
+  },
+  {
+    actionName: 'undo',
+    label: 'Undo',
+    imgSrc: 'images/undo.png',
+  },
+  {
+    actionName: 'redo',
+    label: 'Redo',
+    imgSrc: 'images/redo.png',
+  },
+  {
+    actionName: 'show',
+    label: 'Show',
+    imgSrc: 'images/camera.png',
+  },
+  {
+    actionName: 'print',
+    label: 'Print',
+    imgSrc: 'images/printer.png',
+  },
+  {
+    actionName: 'export',
+    label: 'Export',
+    imgSrc: 'images/export1.png',
+  },
+  {
+    actionName: 'collapseAll',
+    label: 'Collapse All',
+    imgSrc: 'images/navigate_minus.png',
+    isTransparent: true,
+  },
+  {
+    actionName: 'expandAll',
+    label: 'Expand All',
+    imgSrc: 'images/navigate_plus.png',
+    isTransparent: true,
+  },
+  {
+    actionName: 'enterGroup',
+    label: 'Enter',
+    imgSrc: 'images/view_next.png',
+    isTransparent: true,
+  },
+  {
+    actionName: 'exitGroup',
+    label: 'Exit',
+    imgSrc: 'images/view_previous.png',
+    isTransparent: true,
+  },
+  {
+    actionName: 'zoomIn',
+    label: 'Zoon in',
+    imgSrc: 'images/zoom_in.png',
+    isTransparent: true,
+  },
+  {
+    actionName: 'zoomOut',
+    label: 'Zoom out',
+    imgSrc: 'images/zoom_out.png',
+    isTransparent: true,
+  },
+  {
+    actionName: 'actualSize',
+    label: 'Actual size',
+    imgSrc: 'images/view_1_1.png',
+    isTransparent: true,
+  },
+  {
+    actionName: 'fit',
+    label: 'Fit',
+    imgSrc: 'images/fit_to_size.png',
+    isTransparent: true,
+  },
+]
