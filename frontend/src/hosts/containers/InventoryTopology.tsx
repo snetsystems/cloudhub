90a7f02e84c11d34a5c9b7d01a408172c7c86e37
Choe DaeBeom <daebeom.choe@snetsystems.co.kr>
2021-03-02 08:51:45 +0900

    dbchoe: add mxgraph style

--- frontend/src/hosts/containers/InventoryTopology.tsx
+++ frontend/src/hosts/containers/InventoryTopology.tsx
@@ -21,9 +21,9 @@ import _ from 'lodash'
 
 // component
 // import {Button, ButtonShape, IconFont} from 'src/reusable_ui'
-import HostList from 'src/hosts/components/HostList'
+// import HostList from 'src/hosts/components/HostList'
 import {TableBody, TableBodyRowItem} from 'src/addon/128t/reusable/layout'
-import uuid from 'uuid'
+// import uuid from 'uuid'
 // import Tools from 'src/hosts/components/Tools'
 // import Properties from 'src/hosts/components/Properties'
 import FancyScrollbar from 'src/shared/components/FancyScrollbar'
@@ -163,7 +163,6 @@ class InventoryTopology extends PureComponent<Props, State> {
     }
   }
 
-  //
   private draggable = () => {}
 
   //Creates the textfield for the given property.
@@ -501,7 +500,7 @@ class InventoryTopology extends PureComponent<Props, State> {
 
   private topologyEditor = () => {
     const graph = this.graph
-
+    this.configureStylesheet()
     this.addHostButton()
     this.addSidebarButton()
     this.addToolbarButton()
@@ -792,6 +791,83 @@ class InventoryTopology extends PureComponent<Props, State> {
     //     return firstName + ' ' + lastName
     //   }
     // }
+  }
+  private configureStylesheet = () => {
+    let style = new Object()
+    const mxConstants = this.mxConstants
+    style[mxConstants.STYLE_SHAPE] = mxConstants.SHAPE_RECTANGLE
+    style[mxConstants.STYLE_PERIMETER] = this.mx.mxPerimeter.RectanglePerimeter
+    style[mxConstants.STYLE_ALIGN] = mxConstants.ALIGN_CENTER
+    style[mxConstants.STYLE_VERTICAL_ALIGN] = mxConstants.ALIGN_MIDDLE
+    style[mxConstants.STYLE_GRADIENTCOLOR] = '#41B9F5'
+    style[mxConstants.STYLE_FILLCOLOR] = '#8CCDF5'
+    style[mxConstants.STYLE_STROKECOLOR] = '#1B78C8'
+    style[mxConstants.STYLE_FONTCOLOR] = '#000000'
+    style[mxConstants.STYLE_ROUNDED] = true
+    style[mxConstants.STYLE_OPACITY] = '80'
+    style[mxConstants.STYLE_FONTSIZE] = '12'
+    style[mxConstants.STYLE_FONTSTYLE] = 0
+    style[mxConstants.STYLE_IMAGE_WIDTH] = '48'
+    style[mxConstants.STYLE_IMAGE_HEIGHT] = '48'
+    this.graph.getStylesheet().putDefaultVertexStyle(style)
+
+    // NOTE: Alternative vertex style for non-HTML labels should be as
+    // follows. This repaces the above style for HTML labels.
+    /*var style = new Object();
+			style[mxConstants.STYLE_SHAPE] = mxConstants.SHAPE_LABEL;
+			style[mxConstants.STYLE_PERIMETER] = mxPerimeter.RectanglePerimeter;
+			style[mxConstants.STYLE_VERTICAL_ALIGN] = mxConstants.ALIGN_TOP;
+			style[mxConstants.STYLE_ALIGN] = mxConstants.ALIGN_CENTER;
+			style[mxConstants.STYLE_IMAGE_ALIGN] = mxConstants.ALIGN_CENTER;
+			style[mxConstants.STYLE_IMAGE_VERTICAL_ALIGN] = mxConstants.ALIGN_TOP;
+			style[mxConstants.STYLE_SPACING_TOP] = '56';
+			style[mxConstants.STYLE_GRADIENTCOLOR] = '#7d85df';
+			style[mxConstants.STYLE_STROKECOLOR] = '#5d65df';
+			style[mxConstants.STYLE_FILLCOLOR] = '#adc5ff';
+			style[mxConstants.STYLE_FONTCOLOR] = '#1d258f';
+			style[mxConstants.STYLE_FONTFAMILY] = 'Verdana';
+			style[mxConstants.STYLE_FONTSIZE] = '12';
+			style[mxConstants.STYLE_FONTSTYLE] = '1';
+			style[mxConstants.STYLE_ROUNDED] = '1';
+			style[mxConstants.STYLE_IMAGE_WIDTH] = '48';
+			style[mxConstants.STYLE_IMAGE_HEIGHT] = '48';
+			style[mxConstants.STYLE_OPACITY] = '80';
+			graph.getStylesheet().putDefaultVertexStyle(style);*/
+
+    style = new Object()
+    style[mxConstants.STYLE_SHAPE] = mxConstants.SHAPE_SWIMLANE
+    style[mxConstants.STYLE_PERIMETER] = this.mx.mxPerimeter.RectanglePerimeter
+    style[mxConstants.STYLE_ALIGN] = mxConstants.ALIGN_CENTER
+    style[mxConstants.STYLE_VERTICAL_ALIGN] = mxConstants.ALIGN_TOP
+    style[mxConstants.STYLE_FILLCOLOR] = '#FF9103'
+    style[mxConstants.STYLE_GRADIENTCOLOR] = '#F8C48B'
+    style[mxConstants.STYLE_STROKECOLOR] = '#E86A00'
+    style[mxConstants.STYLE_FONTCOLOR] = '#000000'
+    style[mxConstants.STYLE_ROUNDED] = true
+    style[mxConstants.STYLE_OPACITY] = '80'
+    style[mxConstants.STYLE_STARTSIZE] = '30'
+    style[mxConstants.STYLE_FONTSIZE] = '16'
+    style[mxConstants.STYLE_FONTSTYLE] = 1
+    this.graph.getStylesheet().putCellStyle('group', style)
+
+    style = new Object()
+    style[mxConstants.STYLE_SHAPE] = mxConstants.SHAPE_IMAGE
+    style[mxConstants.STYLE_FONTCOLOR] = '#774400'
+    style[mxConstants.STYLE_PERIMETER] = this.mx.mxPerimeter.RectanglePerimeter
+    style[mxConstants.STYLE_PERIMETER_SPACING] = '6'
+    style[mxConstants.STYLE_ALIGN] = mxConstants.ALIGN_LEFT
+    style[mxConstants.STYLE_VERTICAL_ALIGN] = mxConstants.ALIGN_MIDDLE
+    style[mxConstants.STYLE_FONTSIZE] = '10'
+    style[mxConstants.STYLE_FONTSTYLE] = 2
+    style[mxConstants.STYLE_IMAGE_WIDTH] = '16'
+    style[mxConstants.STYLE_IMAGE_HEIGHT] = '16'
+    this.graph.getStylesheet().putCellStyle('port', style)
+
+    style = this.graph.getStylesheet().getDefaultEdgeStyle()
+    style[mxConstants.STYLE_LABEL_BACKGROUNDCOLOR] = '#FFFFFF'
+    style[mxConstants.STYLE_STROKEWIDTH] = '2'
+    style[mxConstants.STYLE_ROUNDED] = true
+    style[mxConstants.STYLE_EDGE] = this.mx.mxEdgeStyle.EntityRelation
   }
 
   private addEditorAction = (editor: mxEditor, graph?: mxGraph) => {
