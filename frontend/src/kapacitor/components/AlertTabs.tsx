fe6d6a45dd3e852e4fe162f979a094b741d400d3
rjm <jaemyeong.roh@snetsystems.co.kr>
2021-04-16 09:23:31 +0900

    jmroh: feat(kapacitor/opsgenie2): add recovery action (#5595)

--- frontend/src/kapacitor/components/AlertTabs.tsx
+++ frontend/src/kapacitor/components/AlertTabs.tsx
@@ -288,6 +288,7 @@ class AlertTabs extends PureComponent<Props, State> {
               configSections,
               AlertTypes.opsgenie2
             )}
+            v2={true}
           />
         )
       case AlertTypes.pagerduty:
