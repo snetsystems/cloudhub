ab7d2c46d3542be77adac09c4872bd2b543a07a4
rjm <jaemyeong.roh@snetsystems.co.kr>
2021-04-20 10:07:13 +0900

    jmroh: feat(ui): reset username and password upon changed authentication scheme

--- frontend/src/sources/components/SourceStep.tsx
+++ frontend/src/sources/components/SourceStep.tsx
@@ -196,7 +196,7 @@ class SourceStep extends PureComponent<Props, State> {
           halfWidth={!onBoarding}
           isChecked={sourceIsV2}
           text={'InfluxDB v2 Auth'}
-          onChange={this.changeVersion}
+          onChange={this.changeAuth}
         />
 
         {this.isHTTPS && (
@@ -268,11 +268,13 @@ class SourceStep extends PureComponent<Props, State> {
     this.setState({source: {...source, [key]: value}})
     setError(false)
   }
-  private changeVersion = (v2: boolean) => {
+  private changeAuth = (v2: boolean) => {
     const {source} = this.state
     this.setState({
       source: {
         ...source,
+        username: '',
+        password: '',
         type: v2 ? SOURCE_TYPE_INFLUX_V2 : SOURCE_TYPE_INFLUX_V1,
         version: v2 ? '2.x' : '1.x',
       },
