b7ae1325e1a2dfc9dc075d326e3239f977a94db3
rjm <jaemyeong.roh@snetsystems.co.kr>
2021-04-23 13:05:54 +0900

    jmroh: chore: remove unused import

--- frontend/src/dashboards/reducers/cellEditorOverlay.ts
+++ frontend/src/dashboards/reducers/cellEditorOverlay.ts
@@ -1,6 +1,3 @@
-// libraries
-import _ from 'lodash'
-
 // actions
 import {Action, ActionType} from 'src/dashboards/actions/cellEditorOverlay'
 
@@ -33,6 +30,4 @@ export default (state = initialState, action: Action): CEOInitialState => {
       return {...state, timeRange}
     }
   }
-
-  return state
 }
