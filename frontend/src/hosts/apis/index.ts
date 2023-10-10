301fb1856b5bb29b7811e0641335d1f909e4cd50
jinhyeong.kim@snetsystems.co.kr <jinhyeong.kim@snetsystems.co.kr>
2023-10-10 13:39:40 +0900

    At #423 Fix Empty Hostname Output when 'hostname' Tag is Missing

--- frontend/src/hosts/apis/index.ts
+++ frontend/src/hosts/apis/index.ts
@@ -1544,16 +1544,16 @@ export const getHostsInfoWithIpmi = async (
   meRole: string
 ): Promise<HostsObject> => {
   const query = replaceTemplate(
-    `SELECT mean("value") AS "ipmiCpu" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE "name" = 'cpu_usage' AND time > now() - 10m GROUP BY hostname fill(null);
-     SELECT mean("value") AS "ipmiMemory" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE "name" = 'mem_usage' AND time > now() - 10m GROUP BY hostname;
-     SHOW TAG VALUES FROM \":db:\".\":rp:\".\"ipmi_sensor\" WITH KEY = "hostname" WHERE TIME > now() - 10m;
-     SELECT max("inlet") AS "inlet" FROM ( SELECT last("value") AS "inlet" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE time > now() - 10m AND ("name" =~ ${new RegExp(
+    `SELECT mean("value") AS "ipmiCpu" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE "name" = 'cpu_usage' AND "hostname" != '' AND time > now() - 10m GROUP BY hostname fill(null);
+     SELECT mean("value") AS "ipmiMemory" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE "name" = 'mem_usage' AND "hostname" != '' AND time > now() - 10m GROUP BY hostname;
+     SHOW TAG VALUES FROM \":db:\".\":rp:\".\"ipmi_sensor\" WITH KEY = "hostname" WHERE TIME > now() - 10m AND "hostname" != '';
+     SELECT max("inlet") AS "inlet" FROM ( SELECT last("value") AS "inlet" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE "hostname" != '' AND time > now() - 10m AND ("name" =~ ${new RegExp(
        /inlet_temp|mb_cpu_in_temp|temp_mb_inlet/
      )}) GROUP BY hostname ) GROUP BY hostname;
-     SELECT max("inside") AS "inside", "name" as "cpu_count" FROM ( SELECT last("value") AS "inside"  FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE time > now() - 10m AND ("name" =~ ${new RegExp(
+     SELECT max("inside") AS "inside", "name" as "cpu_count" FROM ( SELECT last("value") AS "inside"  FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE "hostname" != '' AND time > now() - 10m AND ("name" =~ ${new RegExp(
        /cpu\d+_temp|cpu_temp_\d+|cpu_dimmg\d+_temp|temp_cpu\d+/
      )}) GROUP BY hostname, "name" ) GROUP BY hostname;
-     SELECT max("outlet") AS "outlet" FROM ( SELECT last("value") AS "outlet" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE time > now() - 10m AND ("name" =~ ${new RegExp(
+     SELECT max("outlet") AS "outlet" FROM ( SELECT last("value") AS "outlet" FROM \":db:\".\":rp:\".\"ipmi_sensor\" WHERE "hostname" != '' AND time > now() - 10m AND ("name" =~ ${new RegExp(
        /exhaust_temp|outlet_temp|mb_cpu_out_temp|mb_cpu_out_temp/
      )}) GROUP BY hostname ) GROUP BY hostname;
      `,
