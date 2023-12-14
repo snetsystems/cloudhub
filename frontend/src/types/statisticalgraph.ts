a60aaa3005341877c2054f3821ef74001ce6a46a
jaegeunha <jaegeun.ha@snetsystems.co.kr>
2023-12-14 10:14:43 +0900

    At #444 Add Statistical Graph Type file

---
+++ frontend/src/types/statisticalgraph.ts
@@ -0,0 +1,3 @@
+export type StatisticalGraphScaleType = 'logarithmic' | undefined
+export type StatisticalGraphBoundsType = [string, string] | undefined
+export type StatisticalGraphMinMaxValueType = number | undefined
