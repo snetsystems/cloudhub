83ccea038bb1f0410de6d459f3bae269b118b961
jaegeun <jaegeun.ha@snetsystems.co.kr>
2024-06-25 13:25:33 +0900

    At #471 Apply top padding to Monitoring Modal

--- frontend/src/device_management/constants/systemMsg.tsx
+++ frontend/src/device_management/constants/systemMsg.tsx
@@ -6,27 +6,26 @@ export const MONITORING_MODAL_INFO = {
   ),
   workHeader: <>Here is the Device List you requested.</>,
   monitoringMessage: (
-    <>
+    <span className="span-header">
       This action will{' '}
       <label className="label-warning">pause data collection </label>for about 1
-      minute. Would you like to{' '}
-      <label className="label-warning">continue</label>?
-    </>
+      minute. Would you like to <label className="label-warning">apply</label>?
+    </span>
   ),
   learningMessage: <></>,
   deleteMonitoringMessage: (
-    <>
+    <span className="span-header">
       <label className="label-warning">
         Device you selected is in monitoring status
       </label>
       . Are you sure you want to <label className="label-warning">delete</label>{' '}
       it?
-    </>
+    </span>
   ),
   deleteGeneralMessage: (
-    <>
+    <span className="span-header">
       Are you sure you want to <label className="label-warning">delete</label>{' '}
       the selected device?
-    </>
+    </span>
   ),
 }
