2ae49280759030ac7056bc398c228ddc57b7dd7c
jaeheesnet <jaehee.yun@snetsystems.co.kr>
2024-10-23 05:58:20 +0000

    AlertValueStatus case change

--- frontend/src/shared/utils/alertValueStatus.ts
+++ frontend/src/shared/utils/alertValueStatus.ts
@@ -8,6 +8,12 @@ export const alertValueStatus = (value: number) => {
       return 'DL Predict'
     case 3:
       return 'ML+DL Predict'
+    case 4:
+      return 'In Packet Error'
+    case 5:
+      return 'Out Packet Error'
+    case 6:
+      return 'In/Out Packet Error'
     default:
       return null
   }
