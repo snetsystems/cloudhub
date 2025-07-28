089e3d049b664b49f88113e4f55befbea198d2ce
jaegeunha <jaegeun.ha@snetsystems.co.kr>
2025-07-28 08:32:23 +0000

    At #577 Update LogAnalysis Tip text

--- frontend/src/log_analysis/components/LogAnalysisTips.tsx
+++ frontend/src/log_analysis/components/LogAnalysisTips.tsx
@@ -2,7 +2,7 @@ import React, {FunctionComponent} from 'react'
 import ReactTooltip from 'react-tooltip'
 
 const logAnalysisTipsText =
-  '<h1>Search Tips:</h1><p>Enter your filter using KQL (e.g. <code>message: error</code>, <code>host: "host1"</code>) and <code>click</code> the <strong>Search</strong> button to apply.</p><br/><h1>Tree Map / Tag Cloud Tips:</h1><p><code>Click</code> on keyword to apply it as a filter.</p><br/><h1>Syslog Table Tips:</h1><p><code>Click</code> on column to apply a keyword filter.<br/><code>Clicking</code> the chart icon opens sidebar with time range ±2 hours and selected hostname.</p><br/><h1>Sidebar Tips:</h1><p>Set <strong>Vendor</strong> and <strong>Alias</strong>, then <code>click</code> <strong>Apply</strong>.<br/>Enable <strong>"From Agent"</strong> if agent is installed.</p>'
+  '<h1>Search Tips:</h1><p>Enter your filter using KQL (e.g. <code>message: error</code>, <code>host: "host1"</code>) and <code>click</code> the <strong>Search</strong> button to apply.</p><br/><h1>Tree Map / Tag Cloud Tips:</h1><p><code>Click</code> on keyword to apply a keyword filter.</p><br/><h1>Syslog Table Tips:</h1><p><code>Click</code> on column to apply a keyword filter.<br/><code>Clicking</code> the chart icon opens sidebar with time range ±2 hours and selected hostname.</p><br/><h1>Sidebar Tips:</h1><p>Set <strong>Vendor</strong> and <strong>Alias</strong>, then <code>click</code> <strong>Apply</strong>.<br/>Enable <strong>"From Agent"</strong> if agent is installed.</p>'
 
 const LogAnalysisTips: FunctionComponent = () => (
   <div
