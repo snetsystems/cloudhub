48317b24fe0208379011851d0a75b48e0463f3ee
jaeheesnet <jaehee.yun@snetsystems.co.kr>
2025-08-04 08:32:54 +0000

    At #588 log clear

--- frontend/src/log_analysis/hooks/useDeviceType.ts
+++ frontend/src/log_analysis/hooks/useDeviceType.ts
@@ -28,7 +28,6 @@ export const useDeviceType = (source: Source) => {
           layouts,
           tempVars
         )
-        console.log('tagValues', tagValues)
         setAllTagValues(tagValues)
       } catch (error) {
         console.error('Error fetching dropdown items:', error)
