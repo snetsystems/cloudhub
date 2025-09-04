a36dba3de097b5205fa45af4709ec8da3dd1566b
jaeheesnet <jaehee.yun@snetsystems.co.kr>
2025-09-04 08:04:41 +0000

    At #587 Prediction Hexbin loading make fast through change query

--- frontend/src/device_management/apis/index.ts
+++ frontend/src/device_management/apis/index.ts
@@ -402,12 +402,23 @@ export const getLiveDeviceInfo = async (
 
   const deviceIpList = deviceList.devices.map(i => i.device_ip)
 
+  // const query = replaceTemplate(
+  //   `SELECT mean("cpu1min") FROM \":db:\".\"autogen\".\"snmp_nx\" WHERE time > now() - 5m GROUP BY agent_host;
+  //   SELECT mean("mem_usage") FROM \":db:\".\"autogen\".\"snmp_nx\" WHERE time > now() - 5m GROUP BY agent_host;
+  //   SELECT last("tff_volume") from (SELECT non_negative_derivative(mean("total_ifHCOutOctets"),1s) + non_negative_derivative(mean("total_ifHCInOctets"),1s) AS "tff_volume" FROM \":db:\"."autogen"."snmp_nx" WHERE "time" > now()-5m GROUP BY time(1m), "agent_host") GROUP BY "agent_host";
+
+  //     `,
+  //   tempVars
+  // )
   const query = replaceTemplate(
     `SELECT mean("cpu1min") FROM \":db:\".\"autogen\".\"snmp_nx\" WHERE time > now() - 5m GROUP BY agent_host;
     SELECT mean("mem_usage") FROM \":db:\".\"autogen\".\"snmp_nx\" WHERE time > now() - 5m GROUP BY agent_host;
-    SELECT last("tff_volume") from (SELECT non_negative_derivative(mean("total_ifHCOutOctets"),1s) + non_negative_derivative(mean("total_ifHCInOctets"),1s) AS "tff_volume" FROM \":db:\"."autogen"."snmp_nx" WHERE "time" > now()-5m GROUP BY time(1m), "agent_host") GROUP BY "agent_host";
-   
-      `,
+    SELECT non_negative_derivative(mean("total_ifHCOutOctets"), 1s) + non_negative_derivative(mean("total_ifHCInOctets"), 1s) AS "last_tff_volume"
+    FROM \":db:\".\"autogen\".\"snmp_nx\"
+    WHERE time > now() - 5m
+    GROUP BY time(1m), "agent_host"
+    FILL(none)
+    LIMIT 1;`,
     tempVars
   )
   // SHOW TAG VALUES WITH KEY = "agent_host" WHERE TIME > now() - 10m;
