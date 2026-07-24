ed9a9c5c07908d1999816765a62f8d0e5ac32e1e
heekyung <heekyung.hwang@snetsystems.co.kr>
2026-07-24 01:16:26 +0000

    At #618 Add Query Tips to GraphTips and clarify Submit Query tooltip

--- frontend/src/shared/components/GraphTips.tsx
+++ frontend/src/shared/components/GraphTips.tsx
@@ -2,7 +2,7 @@ import React, {FunctionComponent} from 'react'
 import ReactTooltip from 'react-tooltip'
 
 const graphTipsText =
-  '<h1>Graph Tips:</h1><p><code>Click + Drag</code> Zoom in (X or Y)<br/><code>Shift + Click</code> Pan Graph Window<br/><code>Double Click</code> Reset Graph Window</p><h1>Static Legend Tips:</h1><p><code>Click</code>Focus on single Series<br/><code>Shift + Click</code> Show/Hide single Series</p>'
+  '<h1>Graph Tips:</h1><p><code>Click + Drag</code> Zoom in (X or Y)<br/><code>Shift + Click</code> Pan Graph Window<br/><code>Double Click</code> Reset Graph Window</p><h1>Static Legend Tips:</h1><p><code>Click</code>Focus on single Series<br/><code>Shift + Click</code> Show/Hide single Series</p><h1>Query Tips:</h1><p><code>Shift + Enter</code>Newline<br/><code>Ctrl + Enter</code> Submit Query</p>'
 
 const GraphTips: FunctionComponent = () => (
   <div
